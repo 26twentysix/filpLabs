550280e7a99ab1840e218a6bb1cad388acf73e6f
lil angel <cvndyperov@gmail.com>
2022-05-05 22:29:14 +0500

    task3 done

--- 3rdTask.cpp
+++ 3rdTask.cpp
@@ -12,19 +12,21 @@ private:
     T *objPtr = nullptr;
     vector<void *> objSmartPointers;
 
+    void setPtr(T *newObj) {
+        objPtr = newObj;
+    }
+
     void printPointers() {
-        for (auto & objSmartPointer : objSmartPointers) {
-            cout << "   " << objSmartPointer << "\n";
+        for (auto &objSmartPointer: objSmartPointers) {
+            if (objSmartPointer != nullptr) {
+                cout << "   " << objSmartPointer << "\n";
+            }
         }
     }
 
 public:
     Object(T *newObj) {
         setPtr(newObj);
-    }
-
-    void setPtr(T *newObj) {
-        objPtr = newObj;
     }
 
     T *getObjPtr() {
@@ -41,13 +43,17 @@ public:
 
     void removeSmartPointer(void *smartPtr) {
         objSmartPointers.erase(remove(objSmartPointers.begin(),
-                                           objSmartPointers.end(), smartPtr),
+                                      objSmartPointers.end(), smartPtr),
                                objSmartPointers.end());
+        if (objSmartPointers.empty()) {
+            delete objPtr;
+            delete this;
+        }
     }
 
     void printObjectInfo() {
-        cout << "Object info:\n " << objPtr << " Object(" << *objPtr << ")\n  Pointers count: "<<
-        objSmartPointers.size() << "\n  Pointers list:\n";
+        cout << "Object info:\n " << objPtr << " Object(" << *objPtr << ")\n  Pointers count: " <<
+             objSmartPointers.size() << "\n  Pointers list:\n";
         printPointers();
     }
 };
@@ -59,8 +65,10 @@ private:
     size_t count;
 
     void release(SmartPointer<T> *ptr) {
-        obj->removeSmartPointer(ptr);
-        obj = nullptr;
+        if (obj) {
+            obj->removeSmartPointer(ptr);
+            obj = nullptr;
+        }
     }
 
     void updateCounter() {
@@ -71,6 +79,10 @@ public:
     SmartPointer(Object<T> &obj) {
         this->obj = &obj;
         obj.addSmartPointer(this);
+    }
+
+    SmartPointer(SmartPointer &ptr) {
+        this->obj = ptr.getObj();
     }
 
     Object<T> *getObj() {
@@ -85,6 +97,7 @@ public:
     void release() {
         obj->removeSmartPointer(this);
         obj = nullptr;
+
     }
 
     SmartPointer &operator=(SmartPointer &otherPtr) {
@@ -98,7 +111,7 @@ public:
 
     void printPointerInfo() {
         cout << "Pointer info:\n Pointer: " << this << "\n Points on: " << getObj()->getObjPtr() <<
-        " Object(" << *getObj()->getObjPtr() << ")\n";
+             " Object(" << *getObj()->getObjPtr() << ")\n";
     }
 
     void printObjectInfo() {
@@ -115,24 +128,23 @@ public:
 int main() {
     Object<int> testIntObj(new int(10));
     Object<int> testNewIntObj(new int(42));
-    auto testPtr = new SmartPointer(testIntObj);
-    auto testPtr1 = new SmartPointer(testIntObj);
+    SmartPointer testPtr0(testIntObj);
+    SmartPointer testPtr1(testIntObj);
     SmartPointer testPtr2(testIntObj);
     SmartPointer testPtr3(testNewIntObj);
-    auto testPtr4 = new SmartPointer(testNewIntObj);
-    testPtr->printInfo();
-    testPtr1->printInfo();
+    SmartPointer testPtr4(testNewIntObj);
+    testPtr0.printInfo();
+    testPtr1.printInfo();
     testPtr2.printInfo();
     testPtr3.printInfo();
-    testPtr4->printInfo();
-    cout << "Doing some changes\n\n\n\n";
-    testPtr->release();
-    testPtr = testPtr1;
-    testPtr2 = testPtr3;
-    testPtr->printInfo();
-    testPtr1->printInfo();
+    testPtr4.printInfo();
+    cout << "\n\n\nDoing some changes\n\n\n\n";
+    testPtr0 = *new SmartPointer(testPtr2);
+    testPtr0 = *new SmartPointer(testPtr3);
+    testPtr2 = testPtr4;
+    testPtr0.printInfo();
+    testPtr1.printInfo();
     testPtr2.printInfo();
     testPtr3.printInfo();
-    testPtr4->printInfo();
-
+    testPtr4.printInfo();
 }
